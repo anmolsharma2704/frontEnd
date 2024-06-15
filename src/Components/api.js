8f5273592ed0e89aaef04fa05561da8d05da16be
Anmol <2704anmolsharma@gmail.com>
2024-06-15 18:22:29 +0530

    Added vercel.json

--- src/Components/api.js
+++ src/Components/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api/v1/', // Replace with your backend API base URL
+  baseURL: 'https://backend-ccxzlbq19-anmol-sharmas-projects-0ab94a1a.vercel.app/api/v1/', // Replace with your backend API base URL
   headers: {
     'Content-Type': 'application/json',
   },
